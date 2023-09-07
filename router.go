8cd5f269482c8c3dced1a4957a9d606e5299c51d
Juan Carlos Lumbiarres <jclumbiarres@protonmail.com>
2023-09-07 21:42:04 +0200

    Simplificado de metodos

--- router.go
+++ router.go
@@ -8,89 +8,72 @@ import (
 // Middleware es una función que toma un http.HandlerFunc y devuelve otro http.HandlerFunc.
 type Middleware func(http.HandlerFunc) http.HandlerFunc
 
-// Router es una interfaz que define los métodos para registrar rutas y handlers HTTP.
+// RouteKey es un tipo personalizado para la clave del mapa de rutas.
+type RouteKey struct {
+	Method string
+	Path   string
+}
+
+// Router es una interfaz que define los métodos para registrar rutas y controladores HTTP.
 type Router interface {
-	GET(ruta string, handler http.HandlerFunc, middlewares ...Middleware)
-	POST(ruta string, handler http.HandlerFunc, middlewares ...Middleware)
-	PUT(ruta string, handler http.HandlerFunc, middlewares ...Middleware)
-	DELETE(ruta string, handler http.HandlerFunc, middlewares ...Middleware)
-	PATCH(ruta string, handler http.HandlerFunc, middlewares ...Middleware)
-	OPTIONS(ruta string, handler http.HandlerFunc, middlewares ...Middleware)
+	GET(path string, handler http.HandlerFunc, middlewares ...Middleware)
+	POST(path string, handler http.HandlerFunc, middlewares ...Middleware)
+	PUT(path string, handler http.HandlerFunc, middlewares ...Middleware)
+	DELETE(path string, handler http.HandlerFunc, middlewares ...Middleware)
+	PATCH(path string, handler http.HandlerFunc, middlewares ...Middleware)
+	OPTIONS(path string, handler http.HandlerFunc, middlewares ...Middleware)
 	ServeHTTP(w http.ResponseWriter, req *http.Request)
 }
 
 // LumbiMuxRouter es una estructura que implementa la interfaz Router.
 type LumbiMuxRouter struct {
-	reglas      map[string]map[string]http.HandlerFunc
-	middlewares map[string][]Middleware
+	routes      map[RouteKey]http.HandlerFunc
+	middlewares map[RouteKey][]Middleware
 }
 
 // NewLumbiMux crea una nueva instancia de LumbiMuxRouter.
 func NewLumbiMux() Router {
 	return &LumbiMuxRouter{
-		reglas:      make(map[string]map[string]http.HandlerFunc),
-		middlewares: make(map[string][]Middleware),
+		routes:      make(map[RouteKey]http.HandlerFunc),
+		middlewares: make(map[RouteKey][]Middleware),
 	}
 }
 
-// GET registra una ruta y handler HTTP GET.
-func (r *LumbiMuxRouter) GET(ruta string, handler http.HandlerFunc, middlewares ...Middleware) {
-	if r.reglas["GET"] == nil {
-		r.reglas["GET"] = make(map[string]http.HandlerFunc)
-	}
-	r.reglas["GET"][ruta] = anadeMiddleware(handler, middlewares...)
-	r.middlewares["GET:"+ruta] = middlewares
+// GET registra una ruta y controlador HTTP GET.
+func (r *LumbiMuxRouter) GET(path string, handler http.HandlerFunc, middlewares ...Middleware) {
+	r.registerRoute("GET", path, handler, middlewares...)
 }
 
-// POST registra una ruta y handler HTTP POST.
-func (r *LumbiMuxRouter) POST(ruta string, handler http.HandlerFunc, middlewares ...Middleware) {
-	if r.reglas["POST"] == nil {
-		r.reglas["POST"] = make(map[string]http.HandlerFunc)
-	}
-	r.reglas["POST"][ruta] = anadeMiddleware(handler, middlewares...)
-	r.middlewares["POST:"+ruta] = middlewares
+// POST registra una ruta y controlador HTTP POST.
+func (r *LumbiMuxRouter) POST(path string, handler http.HandlerFunc, middlewares ...Middleware) {
+	r.registerRoute("POST", path, handler, middlewares...)
 }
 
-// PUT registra una ruta y handler HTTP PUT.
-func (r *LumbiMuxRouter) PUT(ruta string, handler http.HandlerFunc, middlewares ...Middleware) {
-	if r.reglas["PUT"] == nil {
-		r.reglas["PUT"] = make(map[string]http.HandlerFunc)
-	}
-	r.reglas["PUT"][ruta] = anadeMiddleware(handler, middlewares...)
-	r.middlewares["PUT:"+ruta] = middlewares
+// PUT registra una ruta y controlador HTTP PUT.
+func (r *LumbiMuxRouter) PUT(path string, handler http.HandlerFunc, middlewares ...Middleware) {
+	r.registerRoute("PUT", path, handler, middlewares...)
 }
 
-// DELETE registra una ruta y handler HTTP DELETE.
-func (r *LumbiMuxRouter) DELETE(ruta string, handler http.HandlerFunc, middlewares ...Middleware) {
-	if r.reglas["DELETE"] == nil {
-		r.reglas["DELETE"] = make(map[string]http.HandlerFunc)
-	}
-	r.reglas["DELETE"][ruta] = anadeMiddleware(handler, middlewares...)
-	r.middlewares["DELETE:"+ruta] = middlewares
+// DELETE registra una ruta y controlador HTTP DELETE.
+func (r *LumbiMuxRouter) DELETE(path string, handler http.HandlerFunc, middlewares ...Middleware) {
+	r.registerRoute("DELETE", path, handler, middlewares...)
 }
 
-// PATCH registra una ruta y handler HTTP PATCH.
-func (r *LumbiMuxRouter) PATCH(ruta string, handler http.HandlerFunc, middlewares ...Middleware) {
-	if r.reglas["PATCH"] == nil {
-		r.reglas["PATCH"] = make(map[string]http.HandlerFunc)
-	}
-	r.reglas["PATCH"][ruta] = anadeMiddleware(handler, middlewares...)
-	r.middlewares["PATCH:"+ruta] = middlewares
+// PATCH registra una ruta y controlador HTTP PATCH.
+func (r *LumbiMuxRouter) PATCH(path string, handler http.HandlerFunc, middlewares ...Middleware) {
+	r.registerRoute("PATCH", path, handler, middlewares...)
 }
 
-// OPTIONS registra una ruta y handler HTTP OPTIONS.
-func (r *LumbiMuxRouter) OPTIONS(ruta string, handler http.HandlerFunc, middlewares ...Middleware) {
-	if r.reglas["OPTIONS"] == nil {
-		r.reglas["OPTIONS"] = make(map[string]http.HandlerFunc)
-	}
-	r.reglas["OPTIONS"][ruta] = anadeMiddleware(handler, middlewares...)
-	r.middlewares["OPTIONS:"+ruta] = middlewares
+// OPTIONS registra una ruta y controlador HTTP OPTIONS.
+func (r *LumbiMuxRouter) OPTIONS(path string, handler http.HandlerFunc, middlewares ...Middleware) {
+	r.registerRoute("OPTIONS", path, handler, middlewares...)
 }
 
-// ServeHTTP maneja las solicitudes HTTP entrantes mediante la coincidencia del método de solicitud y la ruta de URL con un handler registrado.
+// ServeHTTP maneja las solicitudes HTTP entrantes mediante la coincidencia del método de solicitud y la ruta de URL con un controlador registrado.
 func (r *LumbiMuxRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	if handler, ok := r.reglas[req.Method][req.URL.Path]; ok {
-		for _, middleware := range r.middlewares[req.Method+":"+req.URL.Path] {
+	key := RouteKey{Method: req.Method, Path: req.URL.Path}
+	if handler, ok := r.routes[key]; ok {
+		for _, middleware := range r.middlewares[key] {
 			handler = middleware(handler)
 		}
 		handler(w, req)
@@ -99,7 +82,14 @@ func (r *LumbiMuxRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	http.NotFound(w, req)
 }
 
-// anadeMiddleware toma un http.HandlerFunc y una lista de Middleware y devuelve un nuevo http.HandlerFunc que aplica los middleware a la función original.
+// registerRoute registra una nueva ruta y controlador con el método HTTP especificado.
+func (r *LumbiMuxRouter) registerRoute(method string, path string, handler http.HandlerFunc, middlewares ...Middleware) {
+	key := RouteKey{Method: method, Path: path}
+	r.routes[key] = anadeMiddleware(handler, middlewares...)
+	r.middlewares[key] = middlewares
+}
+
+// anadeMiddleware toma un http.HandlerFunc y una lista de Middleware y devuelve un nuevo http.HandlerFunc que aplica los middlewares a la función original.
 func anadeMiddleware(handler http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
 	for i := len(middlewares) - 1; i >= 0; i-- {
 		handler = middlewares[i](handler)
@@ -107,7 +97,7 @@ func anadeMiddleware(handler http.HandlerFunc, middlewares ...Middleware) http.H
 	return handler
 }
 
-// helloHandler es un handler HTTP simple que escribe "¡Hola, mundo!" en la respuesta.
+// helloHandler es un controlador HTTP simple que escribe "¡Hola, mundo!" en la respuesta.
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("¡Hola, mundo!"))
 }
